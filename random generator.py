ae5491c54a37401efb14eaf2ce62a4c3cf27f5a2
TheSolobaid <thesolobaid@gmail.com>
2023-01-09 14:16:49 +0100

    goodbye motherf*cker

---
+++ random generator.py
@@ -0,0 +1,12 @@
+import random
+characters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', ':', ';', '"', '<', '>', '?', '/']
+while True:
+    random_characters = []
+    x = int(input("longeur de la chaine de charactère : "))
+    for i in range(x):
+        random_characters.append(random.choice(characters))
+    print()
+    for i in range(x):
+        print(random_characters[i], end="")
+    print()
+    print()
