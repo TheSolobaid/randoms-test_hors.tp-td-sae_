bf3a365f5477adc8ab8a80c2a75cf7a0fa7881a0
TheSolobaid <thesolobaid@gmail.com>
2023-01-12 11:47:31 +0100

    goodbye motherf*cker

--- test random draw.py
+++ test random draw.py
@@ -1,24 +1,16 @@
 l=[1,5,6,7,9,10,15,14,17,19,20,25,27]
 
-def recherche(list, n):
-    début = 0
-    fin = len(list)
-    trouvé = False
-    while début<=fin and not trouvé :
-        milieu = (début+fin)//2
-        if list[milieu] == n:
-            trouvé=True
-        elif list[milieu]<x:
-            début = milieu+1
-        else:
-            fin = milieu-1
-    if trouvé:
-        return(milieu)
+def rechercheDichotomiqueRecursive(list, n, gauche, droite):
+    if gauche > droite:
+        return f"{n} n'existe pas dans la list"
+    milieu = (gauche + droite) // 2
+    if list[milieu] == n:
+        return milieu
+    elif list[milieu] > n:
+        return rechercheDichotomiqueRecursive(list, n, gauche, milieu-1)
     else:
-        return(f"{n} n'existe pas dans la list")
-
+        return rechercheDichotomiqueRecursive(list, n, milieu+1, droite)
 
 while True:
-    x = int(input("valeur a chercher dans la liste: "))
-    print(recherche(l, x))
-    
+    x = int(input("valeur a chercher: "))
+    print(rechercheDichotomiqueRecursive(l, x , 0 , len(l)))
