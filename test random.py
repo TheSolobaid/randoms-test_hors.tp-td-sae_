2da148b067c91e19772b70c1e7f5700416fa1b97
TheSolobaid <thesolobaid@gmail.com>
2023-01-18 09:50:08 +0100

    goodbye motherf*cker

--- test random.py
+++ test random.py
@@ -1,4 +1,13 @@
-def test(x:int):
-    print("cest bon")
-
-test('a')
+def rechercheDichotomiqueRecursive( list, n, gauche = 0, droite = -1 ):
+    if gauche == droite: 
+        return False
+    else:
+        if droite == -1 : 
+            droite = len(list)-1
+        m = (gauche+droite)//2
+        if list[m] == int(n) :
+            return m
+        elif list[m] > int(n) :
+            return rechercheDichotomiqueRecursive(list, n, gauche, m-1)
+        else :
+            return rechercheDichotomiqueRecursive(list, n, m+1, droite)
