55978bf12beec73e4fa516502591aaacb2ed769d
TheSolobaid <thesolobaid@gmail.com>
2023-01-20 08:53:29 +0100

    bye

--- test random.py
+++ test random.py
@@ -1,22 +1,11 @@
 import numpy as np
-import pandas as pd
-
-def triInsertion(list):
-    swap=True
-    while swap:
-        swap = False
-        for i in range(len(list)):
-            for j in range(i , -1 , -1):                    #recule dans la liste à partire de i jusqu'au début en allant de -1 en -1
-                if list[j]>list[i]:
-                    list[j], list[i] = list[i], list[j]
-                    swap = True
-    return list
-
+import pandas   as pd
+import  matplotlib.pyplot as plt
 def rechercheDichotomiqueRecursive(liste, n, gauche = 0, droite = -2):
     if droite == -2:
             droite = len(liste) - 1
     if gauche > droite:
-        return None
+        return False
     else:
         milieu = (gauche + droite) // 2
         if liste[milieu] == int(n):
@@ -27,6 +16,33 @@ def rechercheDichotomiqueRecursive(liste, n, gauche = 0, droite = -2):
             return rechercheDichotomiqueRecursive(liste, n, milieu + 1, droite)
 
 
+def triFusion(list): 
+    if len(list)>1:
+        m = len(list) // 2     #trouve le milieu de la liste
+        l = list[:m]           #créer une liste l (gauche) avec la partie gauche
+        r = list[m:]           #créer une liste r (droite) avec la partie droite
+        triFusion(l)        
+        triFusion(r)
+        i, j, k = 0, 0, 0
+        while i < len(l) and j < len(r):    # [...] [...]
+            if l[i] < r[j]:
+                list[k] = l [i]
+                i += 1
+            else:
+                list[k] = r[j]
+                j += 1
+            k += 1
+        while i < len(l):                   # [...] []
+            list[k] = l[i]
+            i += 1
+            k += 1
+        while j < len(r):                   # [] [...]
+            list[k] = r[j]
+            j += 1
+            k += 1  
+    return list
+
+
 def tirage(j):
     tirTotal = []
     for i in range(j):
@@ -35,35 +51,24 @@ def tirage(j):
     return pd.DataFrame(tirTotal, columns=['1st nbr','2nd nbr','3rd nbr','4th nbr','5th nbr'])
 
 
-import matplotlib.pyplot as plt
-
-def histogram(df):
-    # Create a dictionary to store the frequency of each number
-    frequency = {i:0 for i in range(1,46)}
-    # Iterate through each row of the dataframe
-    for i in range(len(df)):
-        # Iterate through each number in the row
-        for j in df.iloc[i]:
-            # Increment the frequency of the number
-            frequency[j] += 1
-    # Create a histogram of the frequency of each number
-    plt.bar(frequency.keys(), frequency.values())
+def histogramme(df):
+    totallist=[]
+    for ligne in df.values:
+        totallist += ligne.tolist()
+    histo = [] # Create an empty list to store the histogram data
+    for i in range(len(totallist)): # Iterate through each value in the flattened DataFrame
+        index = rechercheDichotomiqueRecursive(histo, totallist[i]) # Search for the value in the histogram list
+        if index is not False: # If the value is already in the list
+            histo[index][1] += 1 # Increment the count
+        else:
+            histo.append([totallist[i], 1]) # If the value is not in the list, add it with a count of 1
+            histo = triFusion(histo)
+    plt.hist([x[0] for x in histo], bins=[x for x in range(1,46)], weights=[x[1] for x in histo], density=False, histtype='bar', align='mid', color='blue')
     plt.xlabel('Number')
     plt.ylabel('Frequency')
-    plt.title('Frequency of Numbers in Dataframe')
-    plt.show()
-
-iteration = 0
-np.random.seed(int(2))
-# Number of draws
-num_draws = 14
-tir=tirage(num_draws)
-tritotal = []
-for i in range(len(tir)):
-    templist=tir.iloc[i].values.tolist()
-    templist = triInsertion(templist)
-    tir.iloc[i]=templist
-print(tir)
+    plt.title('Lottery Numbers Histogram')
+    plt.show() # Show the chart
 
 
-histogram(tir)
+tir = tirage(14)
+histogramme(tir)
