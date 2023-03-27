f4a20e84f1cd53f8242c0862e3abdb8bd3111777
TheSolobaid <thesolobaid@gmail.com>
2023-03-27 13:59:50 +0200

    jsp

--- testexo.py
+++ testexo.py
@@ -1,11 +1,20 @@
 from random import randint
 
 def simu(n):
+    
     multsept = 0
+   
     for k in range(1, 1+n):
+
         valeur = randint(1 , 1000)
+
         if valeur%7 == 0:
-            multsept += 1
+            multsept = multsept + 1
+
     return multsept/n
 
-print ('tamère')
+
+
+
+
+simu(100)
